9e02aa834f83655733069cf353ca531189a614cc
chichumahesh <maheramachandran@gmail.com>
2020-02-26 05:12:43 -0500

    Added ownerservice

---
+++ src/main/java/com/mahesh/application/ownerservice/OwnerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.ownerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnerserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahesh/application/ownerservice/OwnerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.ownerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
